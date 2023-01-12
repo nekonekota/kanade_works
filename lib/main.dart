fd70c745253987aea67fdef9a50337b8229f0f9d
kanade <chebkana55@gmail.com>
2023-01-12 10:31:58 +0900

    1/12

--- lib/main.dart
+++ lib/main.dart
@@ -36,11 +36,22 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       bottomNavigationBar: MediaQuery.of(context).size.width < 640
           ? BottomNavigationBar(
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 18,
+                fontFamily: 'BM HANNA',
+                fontWeight: FontWeight.bold,
+              ),
+              selectedLabelStyle: const TextStyle(
+                fontSize: 18,
+                fontFamily: 'BM HANNA',
+                fontWeight: FontWeight.bold,
+              ),
               currentIndex: _selectedIndex,
-              unselectedItemColor: Color.fromARGB(255, 0, 0, 0),
-              selectedItemColor: Color.fromARGB(255, 36, 197, 255),
+              unselectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+              selectedItemColor: const Color.fromARGB(255, 36, 197, 255),
               // called when one tab is selected
               onTap: (int index) {
                 setState(() {
@@ -48,9 +59,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 });
               },
               // bottom tab items
-              items: const [
+              items: const <BottomNavigationBarItem>[
                   BottomNavigationBarItem(
-                      icon: Icon(Icons.expand_less), label: 'Top'),
+                    icon: Icon(Icons.expand_less),
+                    label: ('Top'),
+                  ),
                   BottomNavigationBarItem(
                       icon: Icon(Icons.library_books), label: 'Corection'),
                   BottomNavigationBarItem(
@@ -63,30 +76,66 @@ class _HomeScreenState extends State<HomeScreen> {
             NavigationRail(
               onDestinationSelected: (int index) {
                 setState(() {
-                 print(index); // デバッグ用に出力（タップされたボタンによって数値がかわる）
-          if(index == 0){
-            // 上から30pxまで移動
-            controller.animateTo(0.0, curve: Curves.easeOut, duration: const Duration(milliseconds: 300),);
-          }
-          else if(index == 1){
-            // 上から300pxまで移動
-            controller.animateTo(30.0, curve: Curves.easeOut, duration: const Duration(milliseconds: 300),);
-                }
-          else if(index == 2){
-            // 上から300pxまで移動
-            controller.animateTo(50.0, curve: Curves.easeOut, duration: const Duration(milliseconds: 300),);
-                }});
+                  // ignore: avoid_print
+                  print(index); // デバッグ用に出力（タップされたボタンによって数値がかわる）
+                  if (index == 0) {
+                    // 上から30pxまで移動
+                    controller.animateTo(
+                      0.0,
+                      curve: Curves.easeOut,
+                      duration: const Duration(milliseconds: 300),
+                    );
+                  } else if (index == 1) {
+                    // 上から300pxまで移動
+                    controller.animateTo(
+                      30.0,
+                      curve: Curves.easeOut,
+                      duration: const Duration(milliseconds: 300),
+                    );
+                  } else if (index == 2) {
+                    // 上から300pxまで移動
+                    controller.animateTo(
+                      50.0,
+                      curve: Curves.easeOut,
+                      duration: const Duration(milliseconds: 300),
+                    );
+                  }
+                });
               },
               selectedIndex: _selectedIndex,
               destinations: const [
                 NavigationRailDestination(
-                    icon: Icon(Icons.expand_less), label: Text('Top')),
+                    icon: Icon(Icons.expand_less),
+                    label: Text(
+                      'Top',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'BM HANNA',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
                 NavigationRailDestination(
-                    icon: Icon(Icons.library_books), label: Text('Corection')),
+                    icon: Icon(Icons.library_books),
+                    label: Text(
+                      'Corection',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'BM HANNA',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
                 NavigationRailDestination(
-                    icon: Icon(Icons.face), label: Text('Profile')),
+                  icon: Icon(Icons.face),
+                  label: Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'BM HANNA',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ],
-
               labelType: NavigationRailLabelType.all,
               selectedLabelTextStyle: const TextStyle(
                 color: Color.fromARGB(255, 36, 197, 255),
@@ -129,25 +178,31 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class _Keyvisual extends StatelessWidget {
-  const _Keyvisual({super.key});
+  const _Keyvisual();
   @override
   Widget build(BuildContext context) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final double deviceWidth = MediaQuery.of(context).size.width;
+
     return Stack(
       children: [
         Align(
           alignment: Alignment.topCenter,
-          child: Container(
-            margin: const EdgeInsets.only(top: 130),
-            width: 800,
-            height: 500,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black, width: 4)),
+          child: ConstrainedBox(
+            constraints: BoxConstraints.loose(const Size(800,900)),
+            child: Container(
+              margin: const EdgeInsets.only(top: 100),
+              width: deviceWidth * 0.85,
+              height: deviceHeight * 0.7,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black, width: 4)),
+            ),
           ),
         ),
         Container(
-          margin: const EdgeInsets.only(top: 180, left: 120),
-          width: 800,
+          margin: const EdgeInsets.only(top: 180, left: 90),
+          width: 200,
           height: 400,
           color: Colors.white,
         ),
@@ -156,7 +211,7 @@ class _Keyvisual extends StatelessWidget {
           width: double.infinity,
           child: Padding(
             padding: const EdgeInsets.only(
-                top: 220, right: 50, bottom: 0, left: 120),
+                top: 220, right: 80, bottom: 0, left: 90),
             child: Column(
               children: [
                 Container(
@@ -168,7 +223,7 @@ class _Keyvisual extends StatelessWidget {
                       textAlign: TextAlign.left,
                       style: TextStyle(
                         fontFamily: 'BM HANNA',
-                        fontSize: 109,
+                        fontSize: 105,
                         color: Color.fromARGB(255, 50, 200, 255),
                       ),
                     ),
@@ -183,7 +238,7 @@ class _Keyvisual extends StatelessWidget {
                       textAlign: TextAlign.left,
                       style: TextStyle(
                         fontFamily: 'BM HANNA',
-                        fontSize: 100,
+                        fontSize: 98,
                         color: Color.fromARGB(255, 0, 0, 0),
                       ),
                     ),
@@ -217,7 +272,7 @@ Flutterの勉強を始めた学生が、
 }
 
 class _CorectionPage extends StatelessWidget {
-  const _CorectionPage({super.key});
+  const _CorectionPage();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -228,12 +283,12 @@ class _CorectionPage extends StatelessWidget {
 }
 
 class _MyprofilePage extends StatelessWidget {
-  const _MyprofilePage({super.key});
+  const _MyprofilePage();
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      color: Color.fromARGB(255, 137, 174, 203),
+      color: const Color.fromARGB(255, 137, 174, 203),
     );
   }
 }
